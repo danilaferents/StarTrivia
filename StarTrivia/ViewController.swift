6eac1c1165a07a85c03704afc5701b000a3bb521
Danila Ferents <iambpev@gmail.com>
2019-03-23 23:12:41 +0300

    Initial Commit

---
+++ StarTrivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarTrivia
+//
+//  Created by Мой Господин on 23/03/2019.
+//  Copyright © 2019 Danila Ferents. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
